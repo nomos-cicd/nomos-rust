c6e47521a3c86614e90841cd0e5228d0872da175
requizm <mehmet41650@gmail.com>
2024-10-23 03:22:46 +0300

    Better test coverage

--- tests/script_tests.rs
+++ tests/script_tests.rs
@@ -1,10 +1,23 @@
 use std::path::PathBuf;
 
-use nomos_rust::script::Script;
+use nomos_rust::script::{Script, ScriptParameterType};
 
 #[test]
 fn read_yml() {
     let path_buf = PathBuf::from("tests/scripts/test-script.yml");
-    let yaml_script = Script::try_from(path_buf);
-    assert!(yaml_script.is_ok());
+    let script = Script::try_from(path_buf);
+    assert!(script.is_ok());
+    let script = script.unwrap();
+    assert_eq!(script.id, "test-script");
+    assert_eq!(script.parameters.len(), 2);
+    assert_eq!(script.parameters[0].name, "test_param1");
+    assert_eq!(
+        script.parameters[0].default,
+        Some(ScriptParameterType::Number(5))
+    );
+    assert_eq!(script.parameters[1].name, "test_param2");
+    assert_eq!(
+        script.parameters[1].default,
+        Some(ScriptParameterType::Boolean(true))
+    );
 }
