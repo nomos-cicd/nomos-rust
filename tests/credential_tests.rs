5fab8266bfe499d4b9eb1d42e65166f6c2c23800
requizm <mehmet41650@gmail.com>
2024-10-28 21:14:13 +0300

    Remove unnecessary test

--- tests/credential_tests.rs
+++ tests/credential_tests.rs
@@ -1,29 +1,6 @@
 use std::path::PathBuf;
 
-use nomos_rust::credential::{Credential, CredentialType, TextCredentialParameter};
-
-#[test]
-fn create_credential() {
-    let text_credential = TextCredentialParameter {
-        value: "test".to_string(),
-    };
-
-    let yaml_credential = Credential {
-        id: "test-credential".to_string(),
-        read_only: false,
-        value: CredentialType::Text(text_credential),
-    };
-
-    let credential = Credential::try_from(yaml_credential);
-    assert!(credential.is_ok());
-    let credential = credential.unwrap();
-    assert_eq!(credential.id, "test-credential");
-    match &credential.value {
-        CredentialType::Text(_) => {}
-        _ => panic!("Expected text credential"),
-    }
-    assert!(!credential.read_only);
-}
+use nomos_rust::credential::{Credential, CredentialType};
 
 #[test]
 fn read_yml() {
